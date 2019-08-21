c1b0f9181c05c7abbad067620bcafe8425b3d523
Diamond Lewis <findlewis@gmail.com>
2019-08-21 13:53:17 -0500

    Properly cleanup database after tests (#15)
    * Properly cleanup database after tests
    
    * Add prefix to className
    * Add COLLECTION_PREFIX environment variable
    * Add test
    
    * fix tests
    
    * clean up

--- servers/mongo.js
+++ servers/mongo.js
@@ -3,7 +3,7 @@
 const { ParseServer } = require('parse-server');
 const PARSE_CONFIG = require('../src/config');
 
-ParseServer.start({
+const parseServer = ParseServer.start({
   databaseURI: PARSE_CONFIG.MONGODB_URI,
   appId: PARSE_CONFIG.APP_ID,
   serverURL: PARSE_CONFIG.SERVER_URL,
@@ -11,8 +11,16 @@ ParseServer.start({
   masterKey: PARSE_CONFIG.MASTER_KEY,
   javascriptKey: PARSE_CONFIG.JAVASCRIPT_KEY,
   restAPIKey: PARSE_CONFIG.REST_KEY,
+  collectionPrefix: PARSE_CONFIG.COLLECTION_PREFIX,
   verbose: PARSE_CONFIG.VERBOSE,
   mountPath: PARSE_CONFIG.MOUNT_PATH,
   serverStartComplete: () => process.send({ start: true }),
-  serverCloseComplete: () => process.exit(0),
+  serverCloseComplete: async () => await cleanUp(),
 });
+
+async function cleanUp() {
+  if (process.env.CLEAR) {
+    await parseServer.config.databaseController.adapter.deleteAllClasses(true);
+  }
+  process.exit(0);
+}
